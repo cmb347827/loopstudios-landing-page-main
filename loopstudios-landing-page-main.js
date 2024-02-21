616c0b815d60510c40e2a1be7e1c1fdcec5cc457
cmb347827 <v38d422ll621dem@gmail.com>
2024-02-21 01:09:51 -0500

    Added the source files
    I've always used another non-Github folder, up till now , to code all source files. I can switch it up from now on, to be more  aligned with Github desktop that I use .by just using the Github folder.

--- loopstudios-landing-page-main.js
+++ loopstudios-landing-page-main.js
@@ -15,7 +15,15 @@ const onClick = (selector, handler) => {
 
 
 $(window).on('load',function(){
-	  
-	  
+	$("#navbarCollapse").on("show.bs.collapse", function(){
+        //menu is collapsed, show close icon.
+		$('#open').toggleClass('hidden');
+		$('#close').toggleClass('show');
+    });
+	$("#navbarCollapse").on("hide.bs.collapse", function(){
+        $('#close').toggleClass('show');
+		$('#open').toggleClass('hidden');
+		
+    });  
 	  
 });
